974180edf6f232aedeb6483920b803de51255705
Breno Macena Pereira de Souza <breno@i3t.com.br>
2021-07-17 11:39:30 -0300

    controller

--- Dojo.DotNetCore.Challange/Dojo.DotNetCore.Challange.Api/Controllers/WeatherForecastController.cs
+++ Dojo.DotNetCore.Challange/Dojo.DotNetCore.Challange.Api/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,8 @@ namespace Dojo.DotNetCore.Challange.Api.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        private readonly IHubContext<OrderSender> _streaming;
+
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,9 +21,10 @@ namespace Dojo.DotNetCore.Challange.Api.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHubContext<OrderSender> streaming)
         {
             _logger = logger;
+            _streaming = streaming;
         }
 
         [HttpGet]
